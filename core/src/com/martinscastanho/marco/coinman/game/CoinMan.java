dc84f37d69ce919e8c57ffa1e1276c67b8b28ee1
Marco Martins Castanho <marco.castanho@gmail.com>
2020-03-03 15:06:41 +0100

    Add image textures and make the CoinMan walk

--- core/src/com/martinscastanho/marco/coinman/game/CoinMan.java
+++ core/src/com/martinscastanho/marco/coinman/game/CoinMan.java
@@ -2,32 +2,56 @@ package com.martinscastanho.marco.coinman.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture background;
+	Texture[] man;
+
+	int manState;
+
+	int pause = 0;	// because otherwise the man runs way too fast
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("bg.png");
+		man = new Texture[4];
+		man[0] = new Texture("frame-1.png");
+		man[1] = new Texture("frame-2.png");
+		man[2] = new Texture("frame-3.png");
+		man[3] = new Texture("frame-4.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		// this method render() keeps being called on and on and on...
+
+		pause++;
+		// make the man move once every 8 renders
+		if(pause == 8) {
+			pause = 0;
+			manState++;
+
+			if (manState == 4) {
+				manState = 0;
+			}
+		}
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		// now the man appears on top of hte background
+		batch.draw(man[manState], Gdx.graphics.getWidth()/2 - man[manState].getWidth()/2, Gdx.graphics.getHeight()/2 - man[manState ].getHeight()/2);	// divided by 2 to show on the center fo the screen
+
+
+		// in the end, when we have everything we want showing on screen
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
